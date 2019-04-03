9769039ea33c224a91313c95b1f89cb2d847fdb1
mmeirovich <mmeirovich@gmail.com>
2019-04-04 01:51:42 +0300

    Update MIDIMishka.ino

--- MIDIMishka/MIDIMishka.ino
+++ MIDIMishka/MIDIMishka.ino
@@ -18,19 +18,20 @@ typedef struct{
   byte control;
 } pinToControl;
 
-pinToControl pinToButtonMap[] = {{A0, 20}, {A1, 21}, {A2, 22}, {A3, 23}, {A4, 24}};
-pinToControl pinToKnobMap[] = {{A5, 25}, {A6, 26}, {A7, 27}, {A8, 28}, {A9, 29}};
+pinToControl pinToButtonMap[] = {{2, 0, 20}, {3, 0, 21}, {14, 0, 22}, {15, 0, 23}, {16, 0, 24}};
+pinToControl pinToKnobMap[] = {{18, 0, 25}, {19, 0, 26}, {20, 0, 27}, {21, 0, 28}, {10, 0, 29}};
 
-byte buttonsCount = sizeof(pinToButtonMap);
-byte knobsCount = sizeof(pinToKnobMap);
+byte buttonsCount = 5;
+byte knobsCount = 5;
 
 
 
 void setup() {
-  Serial.begin(115200);
+  delay(5000);
+  Serial.begin(9600);
 }
 
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// First parameter is the event type (0x09 = note on, 0x960008 = note off).
 // Second parameter is note-on/note-off, combined with the channel.
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
 // Third parameter is the note number (48 = middle C).
@@ -59,26 +60,21 @@ void controlChange(byte channel, byte control, byte value) {
 
 // Sends 127 to channel 0 to a given control.
 // Use it for stateless buttons
-void sendButtonPress(byte control){
-    controlChange(0, control, 127);
+void sendButtonPress(byte control, int value){
+  Serial.print("      sending button press"); 
+    controlChange(0, control, 127*value);
     MidiUSB.flush();
 }
 
 // Sends given value to channel 0 of a given control
 // Use it for sliders & potentiometers
 void sendValueChange(byte control, byte value){
+  Serial.print("send value change: control ");
+  Serial.print(control);
+  Serial.print(" value ");
+  Serial.print(value);
     controlChange(0, control, value);    
     MidiUSB.flush();
-}
-
-
-
-void readPin(int pinNumber){
-  int pinValue = analogRead(pinNumber);
-  Serial.print("just read pin ");
-  Serial.print(pinNumber);
-  Serial.print(" value ");
-  Serial.println(pinValue);
 }
 
 
@@ -93,41 +89,50 @@ void logPinWithInput(byte pin, int value){
 
 byte knobToMidiKnob(int knobValue){
   double relation = KNOB_MAX / MIDI_MAX;
-   return knobValue / relation;  
+  return knobValue / relation;  
 }
 
 void loop() {
   // read buttons
   Serial.println("reading buttons"); 
+  Serial.print(buttonsCount); 
+  Serial.println(" buttons"); 
+
   for (int i=0; i<buttonsCount; i++){
     int pinValue = digitalRead(pinToButtonMap[i].pin);
-    if (pinValue == HIGH){
-      if (pinToButtonMap[i].pinValue == LOW){
-        sendButtonPress(pinToButtonMap[i].control);  
-        pinToButtonMap[i].pinValue = HIGH;
-      }      
-    }else{
-      pinToButtonMap[i].pinValue = LOW;
-    }
-    
-    logPinWithInput(pinToButtonMap[i].pin, pinValue);
 
+    Serial.print("value read: "); 
+    Serial.println(pinValue); 
+    Serial.print(" value was: "); 
+    Serial.println(pinToButtonMap[i].pinValue); 
     
+    if (pinValue != pinToButtonMap[i].pinValue){
+     
+      Serial.print(" pin value changed"); 
+      pinToButtonMap[i].pinValue = pinValue;
+      sendButtonPress(pinToButtonMap[i].control, pinValue);  
+                 
+    }
+    logPinWithInput(pinToButtonMap[i].pin, pinValue);
   }
 
 
   Serial.println("reading knobs"); 
   // read knobs
   for (int i=0; i<knobsCount; i++){
+    
     int pinValue = analogRead(pinToKnobMap[i].pin);
     if (pinValue != pinToKnobMap[i].pinValue){
       pinToKnobMap[i].pinValue = pinValue;
-
+  
       byte midiValue = knobToMidiKnob(pinValue);
-      sendValueChange(pinToKnobMap[i].control, pinValue);
+      Serial.print(" from pin ");
+      Serial.print(pinToKnobMap[i].pin);
+      sendValueChange(pinToKnobMap[i].control, midiValue);
     }
     
     logPinWithInput(pinToKnobMap[i].pin, pinValue);
   }
 
+    delay(1000);
 }
